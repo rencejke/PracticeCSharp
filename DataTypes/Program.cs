e191f1d0e538b4cc3645cb0d12e5b3c8cb0edd82
rence <99105241+rencejke@users.noreply.github.com>
2024-10-26 21:45:39 +0800

    update fo data types

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -10,8 +10,18 @@ namespace DataTypes
     {
         static void Main(string[] args)
         {
+           
+            //Numeric Data Types
+            int x = 10, 
+                y = 20, 
+                z = 30;
+
             //integer
             int age = 24;
+
+            //int age = declaring a value
+            //=24; = initializing a value
+            age = 50; //assigning a value
             Console.WriteLine(age);
 
             //check the Minimum Value and Max Value
@@ -20,13 +30,38 @@ namespace DataTypes
 
             //long
             long bigNumber = 900000000L;
+            Console.WriteLine(long.MaxValue); 
+            Console.WriteLine(long.MinValue);
 
             //900000000 is an integer to make it a long number you need to put L in the end
             //900000000L
             //int32 for integer and int64 for long
             Console.WriteLine(bigNumber);
 
+            double negative = -55.4D; //to make it a double number you need to put D in the end
+            Console.WriteLine(negative);
+            Console.WriteLine(double.MaxValue); // 1.79769313486232E+308
+            Console.WriteLine(double.MinValue); //- 1.79769313486232E+308
 
+            float precision = 5.000001F; //to make it a float number you need to put F in the end
+            Console.WriteLine(precision);
+            Console.WriteLine(float.MaxValue); //3.402823E+38
+            Console.WriteLine(float.MinValue); //-3.402823E+38
+
+            decimal money = 14.99M; //to make it a decimal number you need to put M in the end
+            Console.WriteLine(money);
+            Console.WriteLine(decimal.MaxValue); //79228162514264337593543950335
+            Console.WriteLine(decimal.MinValue); //- 79228162514264337593543950335
+
+
+            //Text Based Data Types
+            string name = "Clarence";
+            char MiddleInitial = 'D';
+
+            Console.Write("Your Name is ");
+            Console.Write(name);
+            Console.WriteLine();
+            Console.WriteLine(MiddleInitial);
 
             Console.ReadLine();
 
